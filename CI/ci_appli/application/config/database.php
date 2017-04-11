2b715b039de1deeee22e47c71787090f0eb7367b
gloomandria <tojo.andrianaivololona@gmail.com>
2017-04-11 09:43:23 +0300

    test

--- CI/ci_appli/application/config/database.php
+++ CI/ci_appli/application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'pgsql:host=localhost;port=5432;dbname=database_crud',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'username' => 'postgres',
+	'password' => '123456',
+	'database' => 'database_crud',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
